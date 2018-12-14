2c330b9f8820b5f348bd04ce1f99af99656f0f19
runandstaki <runandstaki@gmail.com>
2018-12-13 22:04:38 -0500

    ex06-12,ex06-13_end

--- ex06-13.js
+++ ex06-13.js
@@ -11,14 +11,14 @@ function ThreeSecChange2(){
   setTimeout("imgchange2()",3000);
 }
 function imgchange2() {
-  document.getElementById('img13').src = './image/josai.png';
-}
+  document.getElementById('img13').src = './image/456.png';
+}and
 
-//function ThreeSecBack() { 
+//function ThreeSecBack() {
 //関数imgchange1()を3000ミリ秒間隔で呼び出す 
 //  setInterval("imgback()",3000);
 //}
 
 function imgback() {
-  document.getElementById('img13').src = './image/main@2x.png';
+  document.getElementById('img13').src = './image/123.png';
 }
---
+++ ex07-3.js
@@ -0,0 +1,5 @@
+function add(){
+var x = text.getElementById('input1').value;
+var y = text.getElementById('input2').value;
+alert(x+y);
+}
