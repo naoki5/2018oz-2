8fe0506cb795d93401c4f1c500b9ced522883696
runandstaki <runandstaki@gmail.com>
2018-12-13 21:43:29 -0500

    message

--- ex06-13.js
+++ ex06-13.js
@@ -1,9 +1,24 @@
-var timer;
-function ThreeSecChange()
-{
- timer = setInterval('imgchange3()',3000);
+function ChangeImageAndBack(){
+  
+  imgback();
+  
+  ThreeSecChange2(); 
+  
 }
-function imgchange3(){
-  document.getElementById('gazou2').src =
-    './image/bulbon.gif';
+
+
+function ThreeSecChange2(){
+  setTimeout("imgchange2()",3000);
+}
+function imgchange2() {
+  document.getElementById('img13').src = './image/josai.png';
+}
+
+//function ThreeSecBack() { 
+//関数imgchange1()を3000ミリ秒間隔で呼び出す 
+//  setInterval("imgback()",3000);
+//}
+
+function imgback() {
+  document.getElementById('img13').src = './image/main@2x.png';
 }
