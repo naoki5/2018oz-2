fd34d697ca90eafd7f5120268136706828939453
runandstaki <runandstaki@gmail.com>
2018-12-16 20:52:42 -0500

    ex07-5_end

---
+++ ex07-4.js
@@ -0,0 +1,5 @@
+function abc(){
+  var h =document.getElementById('both1').value;
+  var w = document.getElementById('both2').value;
+  alert(Number(w)/((Number(h)/100)*(Number(h)/100)));
+}
---
+++ ex07-5.js
@@ -0,0 +1,9 @@
+function nazo() {
+  if(document.getElementById('answer').value == '夢' ){
+    alert('あたり！');
+  }else 
+  {if(document.getElementById('answer').value !== '夢' ){
+      alert('はずれ')
+    }
+    }
+}
