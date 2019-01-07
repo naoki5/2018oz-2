30be1cdc26b387dbe0ffa030a0bcd3ecdf28f2e8
runandstaki <runandstaki@gmail.com>
2019-01-06 20:51:32 -0500

    ex07-11

--- ex07-8.js
+++ ex07-8.js
@@ -2,9 +2,13 @@ function nazo2()
 {
  if(document.getElementById('answer2').value == '夢' ){
     alert('あたり！');
-  }else 
-  {if(document.getElementById('answer2').value !== '夢' ){
-      alert('はずれ')
+  }else{
+    if(document.getElementById('answer2').value == 'ゆめ' ){
+      alert('あたり');
+  }else{
+    if(document.getElementById('answer2').value !== '夢' ){
+      alert('はずれ');
+  }
     }
-    }
+  }
 }
---
+++ ex7-11.js
@@ -0,0 +1,5 @@
+function azw(){
+  var s =document.getElementById('both3').value;
+  var t = document.getElementById('both4').value;
+  alert(Number(t)/((Number(s)/100)*(Number(s)/100)));
+}
